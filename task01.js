6549a7bddb415f774761cd8d95722764ef03fc08
hanish <151109119+hanishP3@users.noreply.github.com>
2024-07-13 09:45:25 +0530

    Add files via upload

---
+++ task01.js
@@ -0,0 +1,32 @@
+//Activity 1
+var num = 333;
+console.log(num)
+var str = "This is the first task"
+console.log(str)
+//Activity 2
+const bool = 0;
+console.log(bool)
+//Activity 3
+const name1 = "hanish";
+const num1 = 333;
+const bool1 = 0;
+const obj = {
+    name:"leo",
+    age:99,
+    address:"japan",
+}
+const arr = [1,3,5,7,9]
+
+console.log(typeof(name1))
+console.log(typeof(num1))
+console.log(typeof(bool1))
+console.log(typeof(obj))
+console.log(typeof(arr))
+//Activity 4
+let number = 3377;
+number = 1234;
+console.log(number)
+//Activity 5
+const age = 99;
+age = 44;
+console.log(age)
